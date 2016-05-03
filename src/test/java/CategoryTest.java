c1d1de106c38aeac0e5e7d2ecef0129b4b9b0d50
Joey <joeyarnstein@gmail.com>
2016-05-03 13:03:48 -0700

    categorytest test returning all tasks as array

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
@@ -64,5 +65,16 @@ public class CategoryTest {
     myCategory.save();
     Category savedCategory = Category.find(myCategory.getId());
     assertTrue(myCategory.equals(savedCategory));
+  }
+  @Test
+  public void getTasks_retrievesALlTasksFromDatabase_tasksList() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task firstTask = new Task("Mow the lawn", myCategory.getId());
+    firstTask.save();
+    Task secondTask = new Task("Do the dishes", myCategory.getId());
+    secondTask.save();
+    Task[] tasks = new Task[] { firstTask, secondTask };
+    assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
   }
 }
